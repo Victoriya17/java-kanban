c35c80fc491049e237eeb7e2c08800025b96aa60
Victoria Beliaeva <mia-175@yandex.ru>
2025-10-07 23:16:22 +0300

    Добавила ветку sprint_6-solution. Добавила класс Node. В интерфейс HistoryManager добавила метод remove() и реализовала его в классе InMemoryHistoryManager. Переписала все методы в InMemoryHistoryManager, чтобы он работал со связным списком, добавила методы remove, removeNode, getTasks, linkLast. В классах Task, Epic, Subtask переопределила метод toString. В классе InMemoryHistoryManagerTest переписала метод add, переименовала метод saveOldTask в saveNewTask и переписала логику, добавила методы deleteTask(), removedTasksDoNotKeepOldIds(), removedSubtasksDoNotKeepOldIds(), changeFieldsBySetters().

--- src/com/yandex/app/service/InMemoryHistoryManager.java
+++ src/com/yandex/app/service/InMemoryHistoryManager.java
@@ -2,23 +2,79 @@ package com.yandex.app.service;
 
 import com.yandex.app.model.Task;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Map;
 
 public class InMemoryHistoryManager implements HistoryManager {
 
-    private List<Task> history = new ArrayList<>();
+    public Node<Task> head;
+    protected Node<Task> tail;
+    private final Map<Integer, Node> idToNode = new HashMap<>();
 
     @Override
     public void add(Task task) {
-        history.add(task.copy());
-        if (history.size() > 10) {
-            history.remove(0);
+        if (idToNode.containsKey(task.getId())) {
+            removeNode(idToNode.get(task.getId()));
         }
+        idToNode.put(task.getId(), linkLast(task));
     }
 
     @Override
     public List<Task> getHistory() {
-        return List.copyOf(history);
+        return getTasks();
+    }
+
+    @Override
+    public void remove(int id) {
+        if (idToNode.containsKey(id)) {
+            removeNode(idToNode.get(id));
+            idToNode.remove(id);
+        }
+    }
+
+    private Node<Task> linkLast(Task task) {
+        final Node<Task> oldTail = tail;
+        final Node<Task> newNode = new Node<>(oldTail, task, null);
+        tail = newNode;
+        if (oldTail == null) {
+            head = newNode;
+        } else {
+            oldTail.next = newNode;
+        }
+        return newNode;
+    }
+
+    private List<Task> getTasks() {
+        List<Task> listTasks = new ArrayList<>();
+        Node<Task> current = head;
+        while (current != null) {
+            listTasks.add(current.task);
+            current = current.next;
+        }
+        return listTasks;
+    }
+
+    private void removeNode(Node<Task> nodeToRemove) {
+        if (nodeToRemove == head) {
+            head = head.next;
+            if (head != null) {
+                head.prev = null;
+            } else {
+                tail = null;
+            }
+        } else if (nodeToRemove == tail) {
+            tail = tail.prev;
+            if (tail != null) {
+                tail.next = null;
+            } else {
+                head = null;
+            }
+        } else {
+            nodeToRemove.prev.next = nodeToRemove.next;
+            nodeToRemove.next.prev = nodeToRemove.prev;
+        }
+        idToNode.remove(nodeToRemove.task.getId());
     }
 }
